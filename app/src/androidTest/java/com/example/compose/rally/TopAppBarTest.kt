b16d02db456324de81af50639690b3128f6a8fc4
Antony <vontonnie@gmail.com>
2021-10-06 07:47:04 +0300

    Downgrade the Target to 30

--- app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,4 +1,31 @@
 package com.example.compose.rally
 
+import androidx.compose.ui.test.junit4.createComposeRule
+import com.example.compose.rally.ui.components.RallyTopAppBar
+import org.junit.Rule
+import org.junit.Test
+
+
+//Test Class
 class TopAppBarTest {
+
+    //get compose test rule
+    @get:Rule
+    val composeTestRule= createComposeRule()
+
+    //testing function
+    @Test
+    fun rallyTopAppBarTest() {
+
+        val allScreen = RallyScreen.values().toList()
+        composeTestRule.setContent {
+
+            //the component be tested in isolation goes here
+            RallyTopAppBar(allScreens = allScreen, onTabSelected = {}, currentScreen =RallyScreen.Accounts )
+        }
+
+        //slow down the test
+        Thread.sleep(5000)
+    }
+
 }
